c4a6be7c8958af2458db842de24a8399dce0c682
Helen Ingham <helen_ingham@ajg.com>
2021-09-09 12:04:33 +0100

    amends variable names to be clearer

--- tax_functions.js
+++ tax_functions.js
@@ -1,50 +1,46 @@
- const $ = function (id) {
-        return document.getElementById(id);
-    }
-    
-const calculateTakeHome = function() {
- let total;
- let income = parseFloat( $("income").value );
- $("tax").value =  calcTax(income);
- $("ni").value =  calcNi(income);
- $("net").value =  calcNet(income);
-        
-    function calcTax(amount){
+const $ = function (id) {
+  return document.getElementById(id);
+};
+
+const calculateTakeHome = function () {
+  let total;
+  let grossPay = parseFloat($("grossPay").value);
+  $("tax").value = calcTax(grossPay);
+  $("nationalInsurance").value = calcNationalInsurance(grossPay);
+  $("net").value = calcNet(grossPay);
+
+  function calcTax(grossPay) {
     let calculate = 0;
-    if(amount < 15000){
-        tax = 0;
-    }
-    else if(amount < 50000){
-    tax = (amount - 15000) * .20;
-    }
-    else {
-    tax = 7000 + ((amount - 50000) * .40);
+    if (grossPay < 15000) {
+      tax = 0;
+    } else if (grossPay < 50000) {
+      tax = (grossPay - 15000) * 0.2;
+    } else {
+      tax = 7000 + (grossPay - 50000) * 0.4;
     }
     return parseFloat(tax.toFixed(2));
-    }
- 
-    function calcNi(amount){
-    let calculate = 0;
-    if(amount < 15000){
-        ni = 0;
-    }
-    else if( amount < 50000){
-    ni = (amount - 15000) * .12;    
-    }
-    else{
-    upperNi = (amount - 50000) * .02
-    ni = 4200 + upperNi;
-    }
-    return parseFloat(ni.toFixed(2));
-}
+  }
 
-function calcNet(amount){
-   let calculate = 0;
-   let net = amount - (tax + ni)
-   return parseFloat(net.toFixed(2));
-}
-}
+  function calcNationalInsurance(grossPay) {
+    let calculate = 0;
+    if (grossPay < 15000) {
+      nationalInsurance = 0;
+    } else if (grossPay < 50000) {
+      nationalInsurance = (grossPay - 15000) * 0.12;
+    } else {
+      upperNationalInsurance = (grossPay - 50000) * 0.02;
+      nationalInsurance = 4200 + upperNationalInsurance;
+    }
+    return parseFloat(nationalInsurance.toFixed(2));
+  }
+
+  function calcNet(grossPay) {
+    let calculate = 0;
+    let net = grossPay - (tax + nationalInsurance);
+    return parseFloat(net.toFixed(2));
+  }
+};
 
 window.onload = function () {
-    $("calculate").onclick = calculateTakeHome;
-}
+  $("calculate").onclick = calculateTakeHome;
+};
